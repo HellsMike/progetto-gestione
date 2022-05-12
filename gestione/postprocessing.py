11b538c66dfcd560427032eaf49440793b2a017a
Michele Bianco <bianco.michele@outlook.com>
2022-05-13 01:33:50 +0200

    Add tokenize + lemmatize function

--- gestione/postprocessing.py
+++ gestione/postprocessing.py
@@ -1,11 +1,33 @@
 import json
 import re
+from turtle import down
+from nltk import word_tokenize, download
+from nltk.corpus import stopwords
+import simplemma
+
+
+def tokenize(value):
+    tokens = []
+    langdata = simplemma.load_data("it")
+
+    while True:
+        try:
+            tokenized_value = word_tokenize(value, language="italian")
+            for token in tokenized_value:
+                if not token in stopwords.words("italian") and token.isalnum():
+                    tokens.append(simplemma.lemmatize(token, langdata))
+                    tokens.append(token)
+            return tokens
+        except LookupError:
+            download('punkt')
+            download('stopwords')
 
 
 '''Sistema le collection di documenti'''
 def json_fix():
     giallo_collection = 'giallozafferano.json'
     donna_collection = 'donnamoderna.json'
+    
     for collection in (giallo_collection, donna_collection):
         url = f'static/collections/{collection}'
         
--- gestione/views.py
+++ gestione/views.py
@@ -1,7 +1,12 @@
+import json
 from django.shortcuts import render
-from gestione.postprocessing import json_fix
+from gestione.postprocessing import tokenize
 
 
 def homepage(request):
-
+    url = 'static/collections/giallozafferano.json'
+        
+    with open(url, encoding='utf-8') as file:
+        content = json.load(file)
+    print(tokenize(content[0]['preparazione']))
     return render(request, 'gestione/homepage.html')
