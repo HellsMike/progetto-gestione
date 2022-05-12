fd28ca37f85c09dd49a351a9e7c3d24904deb080
giova <giovannianniballi@hotmail.com>
2022-05-12 18:29:45 +0200

    cutntrim added, it's a primitive way to filter the data #TODO format the file into a proper json

--- gestione/views.py
+++ gestione/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from gestione.postprocessing import json_fix
+from gestione.postprocessing import json_fix, cutntrim
 
 
 def homepage(request):
     json_fix()
+    cutntrim()
     return render(request, 'gestione/homepage.html')
