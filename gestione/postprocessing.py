ee383dd7c8d9448e620edf3d708a50364b687e03
giova <giovannianniballi@hotmail.com>
2022-05-13 02:09:24 +0200

    indexing def added is required to test it!!

--- gestione/postprocessing.py
+++ gestione/postprocessing.py
@@ -1,9 +1,11 @@
 import json
+import os
 import re
-from turtle import down
 from nltk import word_tokenize, download
 from nltk.corpus import stopwords
 import simplemma
+from whoosh import index
+from whoosh.fields import Schema, STORED, TEXT
 
 
 def tokenize(value):
@@ -14,13 +16,38 @@ def tokenize(value):
         try:
             tokenized_value = word_tokenize(value, language="italian")
             for token in tokenized_value:
-                if not token in stopwords.words("italian") and token.isalnum():
+                if token not in stopwords.words("italian") and token.isalnum():
                     tokens.append(simplemma.lemmatize(token, langdata))
                     tokens.append(token)
             return tokens
         except LookupError:
             download('punkt')
             download('stopwords')
+
+
+def indexing():
+    giallo_collection = 'giallozafferano.json'
+    donna_collection = 'donnamoderna.json'
+    schema = Schema(fonte=STORED, categoria=TEXT(stored=True), titolo=TEXT(stored=True, field_boost=2.0),
+                    ricetta_link=STORED, ingredienti=STORED, ingredienti_token=TEXT(stored=False), preparazione=STORED,
+                    immagine_src=STORED)
+
+    if not os.path.exists('static/index_lemm_boost'):
+        os.mkdir('static/index_lemm_boost')
+
+    ix = index.create_in('static/index_lemm_boost', schema)
+    writer = ix.writer(limitmb=2048, procs=4)
+
+    for collection in (giallo_collection, donna_collection):
+        with open(f'static/collections/{collection}') as file:
+            content = json.load(file)
+        for recipe in content:
+            writer.add_document(fonte=recipe['fonte'], categoria=recipe['categoria'], titolo=f"{recipe['titolo']}",
+                                ricetta_link=recipe['ricetta_link'], ingredienti=recipe['ingredienti'],
+                                ingredienti_token=tokenize(recipe['ingredienti_token']),
+                                preparazione=recipe['preparazione'], immagine_src=recipe['immagine_src'])
+
+    writer.commit()
 
 
 '''Sistema le collection di documenti'''
@@ -40,7 +67,8 @@ def json_fix():
             for key in ('ingredienti', 'ingredienti_token', 'preparazione'):
                 values = document[key].replace('\\t', '').replace('\\n', ' ')
                 values = re.sub(' +', ' ', values)
-                values = json.loads(values) #Legge i valori dei campi (dizionario) come json per unirli in una singola stringa
+                # Legge i valori dei campi (dizionario) come json per unirli in una singola stringa
+                values = json.loads(values)
                 new_string = ''
 
                 for value in values:
