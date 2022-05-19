3e7ee4c7dcadbce3bf3242e90aec7d79d5b6f406
Michele Bianco <bianco.michele@outlook.com>
2022-05-19 14:48:09 +0200

    Change grapichal recipe source

--- gestione/templatetags/gestione_tags.py
+++ gestione/templatetags/gestione_tags.py
@@ -12,3 +12,11 @@ def process_ingredients(ingredients):
 @register.filter
 def normalize_url(url):
     return ''.join(filter(str.isalnum, url))
+
+
+@register.filter
+def fancy_source(url):
+    if url == 'https://www.giallozafferano.it/':
+        return 'Giallo Zafferano'
+    if url == 'https://ricette.donnamoderna.com/':
+        return 'Donna Moderna Food'
