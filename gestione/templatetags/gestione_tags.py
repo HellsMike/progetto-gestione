635f1f3c2e1fb591742705d3f1029a118bbb8263
Michele Bianco <bianco.michele@outlook.com>
2022-05-19 02:34:53 +0200

    Update filters

--- gestione/templatetags/gestione_tags.py
+++ gestione/templatetags/gestione_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter
 def process_ingredients(ingredients):
     return ingredients.split(', ')
+
+
+@register.filter
+def normalize_url(url):
+    return ''.join(filter(str.isalnum, url))
